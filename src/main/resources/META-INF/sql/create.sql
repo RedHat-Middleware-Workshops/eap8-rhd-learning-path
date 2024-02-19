7ee7f0cbffc65fbc6cf84e2de41f11336f3c3d1d
Philip Hayes <phayes@phayes-thinkpadp1gen5.remote.csb>
2024-02-19 14:59:33 -0500

    fix to table name

--- src/main/resources/META-INF/sql/create.sql
+++ src/main/resources/META-INF/sql/create.sql
@@ -1,2 +1,2 @@
-create table CONTACT (id serial,first_name varchar(255),last_name varchar(255),company_name varchar(255),phone_number varchar(255), email varchar(255),primary key (email));
+create table contact (id serial,first_name varchar(255),last_name varchar(255),company_name varchar(255),phone_number varchar(255), email varchar(255),primary key (email));
 
