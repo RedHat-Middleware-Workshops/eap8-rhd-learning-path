05d7fc5c19f01793278b35e0e1ad049f15e8e071
Philip Hayes <phayes@phayes-thinkpadp1gen5.remote.csb>
2024-02-19 14:50:39 -0500

    Added sample data

--- src/main/resources/META-INF/sql/create.sql
+++ src/main/resources/META-INF/sql/create.sql
@@ -1,2 +1,2 @@
-create table CONTACT (id int8 not null,first_name varchar(255),last_name varchar(255),company_name varchar(255),phone_number varchar(255), email varchar(255),primary key (email));
+create table CONTACT (id int8 serial,first_name varchar(255),last_name varchar(255),company_name varchar(255),phone_number varchar(255), email varchar(255),primary key (email));
 
