6237e09b06828bf215174ef565df07d41525e3f9
Philip Hayes <phayes@phayes-thinkpadp1gen5.remote.csb>
2024-02-19 14:11:08 -0500

    Added sample data

--- src/main/resources/db/migration/V1_2__AddInitialData.sql
+++ src/main/resources/db/migration/V1_2__AddInitialData.sql
@@ -1,1 +1,2 @@
--- insert into INVENTORY (itemId, link, location, quantity) values ('329299', 'http://maps.google.com/?q=Raleigh', 'Raleigh', 736);
+insert into contact (company_name, email, first_name, last_name, phone_number) values ('ACME', 'sally@example.com','Sally', 'Smith', 8945613411);
+insert into contact (company_name, email, first_name, last_name, phone_number) values ('ACME', 'tom@example.com','Tom', 'Jones', 7929635622);
